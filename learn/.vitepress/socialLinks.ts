89f3ae451d57542b621d92d429a0a4089ecf7d9f
jinzhongjia <mail@nvimer.org>
2023-09-14 16:37:12 +0800

    更新github链接

--- learn/.vitepress/socialLinks.ts
+++ learn/.vitepress/socialLinks.ts
@@ -1,5 +1,5 @@
 import { DefaultTheme } from "vitepress";
 
 export default [
-  { icon: "github", link: "https://github.com/jinzhongjia/learnzig" },
+	{ icon: "github", link: "https://github.com/learnzig/learnzig" },
 ] as DefaultTheme.SocialLink[];
