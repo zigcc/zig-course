f16b76816a8142c3286335881e72c215c479cb19
jinzhongjia <mail@nvimer.org>
2023-10-12 16:52:27 +0800

    增加联合类型章节

--- learn/.vitepress/sidebar.ts
+++ learn/.vitepress/sidebar.ts
@@ -1,177 +1,178 @@
 import { DefaultTheme } from "vitepress";
 
 export default [
-  {
-    text: "前言",
-    link: "/prologue",
-  },
-  {
-    text: "什么是 Zig ？",
-    link: "/what-is-zig",
-  },
+	{
+		text: "前言",
+		link: "/prologue",
+	},
+	{
+		text: "什么是 Zig ？",
+		link: "/what-is-zig",
+	},
 
-  {
-    text: "环境配置",
-    items: [
-      {
-        text: "安装 Zig 环境",
-        link: "/environment/install-environment",
-      },
-      {
-        text: "编辑器选择",
-        link: "/environment/editor",
-      },
-      {
-        text: "zig 命令",
-        link: "/environment/zig-command",
-      },
-    ],
-  },
-  {
-    text: "Hello World",
-    link: "/basic/hello-world",
-  },
-  {
-    text: "基础学习",
-    items: [
-      {
-        text: "变量声明",
-        link: "/basic/define-variable",
-      },
+	{
+		text: "环境配置",
+		items: [
+			{
+				text: "安装 Zig 环境",
+				link: "/environment/install-environment",
+			},
+			{
+				text: "编辑器选择",
+				link: "/environment/editor",
+			},
+			{
+				text: "zig 命令",
+				link: "/environment/zig-command",
+			},
+		],
+	},
+	{
+		text: "Hello World",
+		link: "/basic/hello-world",
+	},
+	{
+		text: "基础学习",
+		items: [
+			{
+				text: "变量声明",
+				link: "/basic/define-variable",
+			},
 
-      {
-        text: "基本类型",
-        collapsed: true,
-        items: [
-          {
-            text: "数值类型",
-            link: "/basic/basic_type/number",
-          },
-          {
-            text: "字符与布尔值",
-            link: "/basic/basic_type/char-and-boolean",
-          },
-          {
-            text: "函数",
-            link: "/basic/basic_type/function",
-          },
-        ],
-      },
-      {
-        text: "高级类型",
-        collapsed: true,
-        items: [
-          {
-            text: "数组",
-            link: "/basic/advanced_type/array",
-          },
-          {
-            text: "指针",
-            link: "/basic/advanced_type/pointer",
-          },
-          {
-            text: "切片",
-            link: "/basic/advanced_type/silce",
-          },
-          {
-            text: "结构体",
-            link: "/basic/advanced_type/struct",
-          },
-          {
-            text: "枚举",
-            link: "/basic/advanced_type/enum",
-          },
-        ],
-      },
-      {
-        text: "流程控制",
-        collapsed: true,
-        items: [
-          {
-            text: "条件",
-            link: "/basic/process_control/decision",
-          },
-          {
-            text: "循环",
-            link: "/basic/process_control/loop",
-          },
-          {
-            text: "switch匹配",
-            link: "/basic/process_control/switch",
-          },
-        ],
-      },
-      {
-        text: "联合类型",
-        link: "/basic/union",
-      },
-      {
-        text: "可选类型",
-        link: "/basic/optional_type",
-      },
-      {
-        text: "错误处理",
-        link: "/basic/error_handle",
-      },
-    ],
-  },
-  {
-    text: "进阶学习",
-    items: [
-      {
-        text: "类型转换",
-        link: "/advanced/type_cast",
-      },
-      {
-        text: "内存管理",
-        link: "/advanced/memory_manage",
-      },
-      {
-        text: "异步",
-        link: "/advanced/async",
-      },
-      {
-        text: "模块系统",
-        link: "/advanced/module-system",
-      },
-      {
-        text: "编译期",
-        link: "/advanced/comptime",
-      },
-      {
-        text: "汇编",
-        link: "/advanced/assembly",
-      },
-      {
-        text: "与 C 交互",
-        link: "/advanced/interact-with-c",
-      },
-    ],
-  },
-  {
-    text: "工程化",
-    items: [
-      {
-        text: "构建系统",
-        link: "/engineering/build-system",
-      },
-    ],
-  },
-  {
-    text: "附录",
-    items: [
-      // {
-      //   text: "后记",
-      //   link: "/epilogue",
-      // },
-      {
-        text: "社区",
-        link: "/appendix/community",
-      },
-      {
-        text: "第三方库",
-        link: "/appendix/well-known-lib",
-      },
-    ],
-  },
+			{
+				text: "基本类型",
+				collapsed: true,
+				items: [
+					{
+						text: "数值类型",
+						link: "/basic/basic_type/number",
+					},
+					{
+						text: "字符与布尔值",
+						link: "/basic/basic_type/char-and-boolean",
+					},
+					{
+						text: "函数",
+						link: "/basic/basic_type/function",
+					},
+				],
+			},
+			{
+				text: "高级类型",
+				collapsed: true,
+				items: [
+					{
+						text: "数组",
+						link: "/basic/advanced_type/array",
+					},
+					{
+						text: "指针",
+						link: "/basic/advanced_type/pointer",
+					},
+					{
+						text: "切片",
+						link: "/basic/advanced_type/silce",
+					},
+					{
+						text: "结构体",
+						link: "/basic/advanced_type/struct",
+					},
+					{
+						text: "枚举",
+						link: "/basic/advanced_type/enum",
+					},
+				],
+			},
+			{
+				text: "联合类型",
+				link: "/basic/union",
+			},
+			{
+				text: "流程控制",
+				collapsed: true,
+				items: [
+					{
+						text: "条件",
+						link: "/basic/process_control/decision",
+					},
+					{
+						text: "循环",
+						link: "/basic/process_control/loop",
+					},
+					{
+						text: "switch匹配",
+						link: "/basic/process_control/switch",
+					},
+				],
+			},
+
+			{
+				text: "可选类型",
+				link: "/basic/optional_type",
+			},
+			{
+				text: "错误处理",
+				link: "/basic/error_handle",
+			},
+		],
+	},
+	{
+		text: "进阶学习",
+		items: [
+			{
+				text: "类型转换",
+				link: "/advanced/type_cast",
+			},
+			{
+				text: "内存管理",
+				link: "/advanced/memory_manage",
+			},
+			{
+				text: "异步",
+				link: "/advanced/async",
+			},
+			{
+				text: "模块系统",
+				link: "/advanced/module-system",
+			},
+			{
+				text: "编译期",
+				link: "/advanced/comptime",
+			},
+			{
+				text: "汇编",
+				link: "/advanced/assembly",
+			},
+			{
+				text: "与 C 交互",
+				link: "/advanced/interact-with-c",
+			},
+		],
+	},
+	{
+		text: "工程化",
+		items: [
+			{
+				text: "构建系统",
+				link: "/engineering/build-system",
+			},
+		],
+	},
+	{
+		text: "附录",
+		items: [
+			// {
+			//   text: "后记",
+			//   link: "/epilogue",
+			// },
+			{
+				text: "社区",
+				link: "/appendix/community",
+			},
+			{
+				text: "第三方库",
+				link: "/appendix/well-known-lib",
+			},
+		],
+	},
 ] as DefaultTheme.Sidebar;
--- learn/basic/union.md
+++ learn/basic/union.md
@@ -19,6 +19,9 @@ const Payload = union {
 
 var payload = Payload{ .int = 1234 };
 
+// 或者是让 zig 编译期进行推倒
+var payload_1: Payload = .{ .int = 1234 };
+
 print("{}\n",.{payload.int});
 ```
 
@@ -34,8 +37,67 @@ const Payload = union {
 
 pub fn main() !void {
     var payload = Payload{ .int = 1234 };
-
+    var payload_1: Payload = .{ .int = 1234 };
+    
     print("{}\n", .{payload.int});
 }
 ```
+
 :::
+
+:::info
+
+需要注意的是，zig 不保证普通联合类型在内存中的表现形式！如果有需要，可以使用 `extern union` 或者 `packed union` 来保证它遵守 c 的规则。
+
+:::
+
+## 枚举标记
+
+联合类型可以在定义时使用枚举进行标记，类似[标记枚举](advanced_type/enum#标记类型)。你可以通过 `@as` 函数将联合类型作为生命的枚举来使用。
+
+示例
+
+:::code-group
+
+```zig [default]
+const ComplexTypeTag = enum {
+    ok,
+    not_ok,
+};
+
+const ComplexType = union(ComplexTypeTag) {
+    ok: u8,
+    not_ok: void,
+};
+```
+
+```zig [more]
+const std = @import("std");
+const expect = std.testing.expect;
+
+const ComplexTypeTag = enum {
+    ok,
+    not_ok,
+};
+const ComplexType = union(ComplexTypeTag) {
+    ok: u8,
+    not_ok: void,
+};
+
+pub fn main() !void {
+    const c = ComplexType{ .ok = 42 };
+    try expect(@as(ComplexTypeTag, c) == ComplexTypeTag.ok);
+}
+```
+
+:::
+
+## `extern union`
+
+`extern union` 保证内存布局与目标 C ABI 兼容。
+
+具体可以见 [`extern struct`](advanced_type/struct.md#extern)。
+
+## `packed union`
+
+`packed union` 保证内存布局和声明顺序相同并且尽量紧凑，具体见 [`extern struct`](advanced_type/struct.md#packed)。
