28305a5eccdd2effda3e5adda6ef3614842fcc2f
“âjinzhongjia <mail@nvimer.org>
2023-12-16 10:13:21 +0800

    增加评论功能

--- learn/.vitepress/theme/index.ts
+++ learn/.vitepress/theme/index.ts
@@ -1,5 +1,34 @@
 // .vitepress/theme/index.js
-import DefaultTheme from "vitepress/theme";
-import "./style.css";
+import DefaultTheme from 'vitepress/theme';
+import giscusTalk from 'vitepress-plugin-comment-with-giscus';
+import { useData, useRoute } from 'vitepress';
 
-export default DefaultTheme;
+export default {
+    ...DefaultTheme,
+    enhanceApp(ctx) {
+        DefaultTheme.enhanceApp(ctx);
+    },
+    setup() {
+        // Get frontmatter and route
+        const { frontmatter } = useData();
+        const route = useRoute();
+        
+        // Obtain configuration from: https://giscus.app/
+        giscusTalk({
+            repo: 'learnzig/learnzig',
+            repoId: 'R_kgDOKRsb5Q',
+            category: 'Comments', // default: `General`
+            categoryId: 'DIC_kwDOKRsb5c4Cbx2i',
+            mapping: 'pathname', // default: `pathname`
+            inputPosition: 'top', // default: `top`
+            lang: 'zh-CN', // default: `zh-CN`
+            strict: "1",
+            reactionsEnabled: "1",
+            theme: "preferred_color_scheme",
+        }, {
+            frontmatter, route
+        },
+            true
+        );
+    }
+};
