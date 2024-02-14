482e96034ba69f7d8766b6b95b25623e71dd0881
jinzhongjia <mail@nvimer.org>
2024-02-14 20:18:16 +0800

    update echo_tcp_server

--- course/code/11/echo_tcp_server/build.zig
+++ course/code/11/echo_tcp_server/build.zig
@@ -1,10 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    var target: std.zig.CrossTarget = .{
-        .os_tag = .windows,
-    };
-    // const target = b.standardTargetOptions(.{});
+    const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
--- course/code/11/echo_tcp_server/src/main.zig
+++ course/code/11/echo_tcp_server/src/main.zig
@@ -1,28 +1,78 @@
 const std = @import("std");
+const builtin = @import("builtin");
 const net = std.net;
 const windows = std.os.windows;
 
+const poll =
+    if (builtin.os.tag == .windows)
+    std.os.windows.poll
+else if (builtin.os.tag == .linux)
+    std.os.linux.poll
+else
+    @compileError("not support current system");
+
+const pollfd =
+    if (builtin.os.tag == .windows)
+    std.os.windows.ws2_32.pollfd
+else if (builtin.os.tag == .linux)
+    std.os.linux.pollfd
+else
+    @compileError("not support current system");
+
 const max_sockets = 1000;
-const POLLRDNORM: i16 = 0x0100;
-const POLLERR: i16 = 0x0001;
-const POLLHUP: i16 = 0x0002;
-const POLLNVAL: i16 = 0x0004;
+
+const POLLIN: i16 =
+    if (builtin.os.tag == .windows)
+    0x0100
+else if (builtin.os.tag == .linux)
+    0x0001
+else
+    @compileError("not support current system");
+
+const POLLERR: i16 =
+    if (builtin.os.tag == .windows)
+    0x0001
+else if (builtin.os.tag == .linux)
+    0x0008
+else
+    @compileError("not support current system");
+const POLLHUP: i16 =
+    if (builtin.os.tag == .windows)
+    0x0002
+else if (builtin.os.tag == .linux)
+    0x0010
+else
+    @compileError("not support current system");
+
+const POLLNVAL: i16 =
+    if (builtin.os.tag == .windows)
+    0x0004
+else if (builtin.os.tag == .linux)
+    0x0020
+else
+    @compileError("not support current system");
+
+const INVALID_SOCKET =
+    if (builtin.os.tag == .windows)
+    std.os.windows.ws2_32.INVALID_SOCKET
+else if (builtin.os.tag == .linux)
+    -1
+else
+    @compileError("not support current system");
 
 pub fn main() !void {
     const address = try net.Address.parseIp4("127.0.0.1", 8080);
     var server = net.StreamServer.init(net.StreamServer.Options{ .reuse_port = true });
     defer server.deinit();
-
     try server.listen(address);
 
-    var sockfds: [max_sockets]windows.ws2_32.pollfd = undefined;
-    var connections: [max_sockets]?net.StreamServer.Connection = undefined;
-
     var buf: [1024]u8 = std.mem.zeroes([1024]u8);
+    var connections: [max_sockets]?net.StreamServer.Connection = undefined;
+    var sockfds: [max_sockets]pollfd = undefined;
 
     for (0..max_sockets) |i| {
-        sockfds[i].fd = windows.ws2_32.INVALID_SOCKET;
-        sockfds[i].events = POLLRDNORM;
+        sockfds[i].fd = INVALID_SOCKET;
+        sockfds[i].events = POLLIN;
         connections[i] = null;
     }
     if (server.sockfd) |fd| {
@@ -34,7 +84,7 @@ pub fn main() !void {
     std.log.info("start listening", .{});
 
     while (true) {
-        var nums = windows.poll(&sockfds, max_sockets, -1);
+        var nums = poll(&sockfds, max_sockets, -1);
         if (nums == 0) {
             continue;
         }
@@ -47,16 +97,16 @@ pub fn main() !void {
                 break;
             }
             const sockfd = sockfds[i];
-            if (sockfd.fd == windows.ws2_32.INVALID_SOCKET) {
+            if (sockfd.fd == INVALID_SOCKET) {
                 continue;
             }
-            if (sockfd.revents & (POLLRDNORM) != 0) {
+            if (sockfd.revents & (POLLIN) != 0) {
                 const c = connections[i];
                 if (c) |connection| {
                     const len = try connection.stream.read(&buf);
                     if (len == 0) {
                         connection.stream.close();
-                        sockfds[i].fd = windows.ws2_32.INVALID_SOCKET;
+                        sockfds[i].fd = INVALID_SOCKET;
                         connections[i] = null;
                         std.log.info("client {} close", .{i});
                     } else {
@@ -64,7 +114,7 @@ pub fn main() !void {
                     }
                 }
             } else if (sockfd.revents & (POLLNVAL | POLLERR | POLLHUP) != 0) {
-                sockfds[i].fd = windows.ws2_32.INVALID_SOCKET;
+                sockfds[i].fd = INVALID_SOCKET;
                 connections[i] = null;
                 std.log.info("client {} close", .{i});
             }
@@ -72,10 +122,10 @@ pub fn main() !void {
             nums -= 1;
         }
 
-        if (sockfds[0].revents & POLLRDNORM != 0 and nums > 0) {
+        if (sockfds[0].revents & POLLIN != 0 and nums > 0) {
             const client = try server.accept();
             for (1..max_sockets) |i| {
-                if (sockfds[i].fd == windows.ws2_32.INVALID_SOCKET) {
+                if (sockfds[i].fd == INVALID_SOCKET) {
                     sockfds[i].fd = client.stream.handle;
                     connections[i] = client;
                     std.log.info("new client {} comes", .{i});
@@ -88,5 +138,7 @@ pub fn main() !void {
         }
     }
 
-    try windows.ws2_32.WSACleanup();
+    if (builtin.os.tag == .windows) {
+        try windows.ws2_32.WSACleanup();
+    }
 }
