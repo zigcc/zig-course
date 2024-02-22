e8d495447a2c85e20306556fea93ebb3e5d39f0f
jinzhongjia <mail@nvimer.org>
2024-02-22 21:12:46 +0800

    更新依赖，移除imgviewer（暂时失效）

--- course/.vitepress/theme/index.ts
+++ course/.vitepress/theme/index.ts
@@ -1,12 +1,8 @@
 // .vitepress/theme/index.js
 import DefaultTheme from "vitepress/theme";
 
-import "viewerjs/dist/viewer.min.css";
-import imageViewer from "./ImgViewer";
-
 import giscus from "./giscus";
 
-import { useRoute } from "vitepress";
 import { h } from "vue";
 
 import RegisterSW from "./components/RegisterSW.vue";
@@ -21,11 +17,5 @@ export default {
       "doc-after": () => h(giscus),
       "layout-bottom": () => h(RegisterSW),
     });
-  },
-  setup() {
-    const route = useRoute();
-
-    // imageView
-    imageViewer(route);
   },
 };
