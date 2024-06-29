5ef3677df1e6dbe11f31406fec4e4414d581eacd
jinzhongjia <mail@nvimer.org>
2024-06-29 20:26:51 +0800

    [代码分割]: 分割 C++ 构建示例部分，移除 C 代码构建

--- course/code/12/build_system/build.zig
+++ course/code/12/build_system/build.zig
@@ -3,19 +3,6 @@ const ChildProcess = std.process.Child;
 
 const args = [_][]const u8{ "zig", "build" };
 
-const sys = [_][]const u8{
-    "basic",
-    "cli",
-    "docs",
-    "embedfile",
-    "externalfile",
-    "lib",
-    "options",
-    "step",
-    "system_lib",
-    "test",
-};
-
 pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
     // #region crossTarget
@@ -47,31 +34,60 @@ pub fn build(b: *std.Build) !void {
 
     b.installArtifact(exe);
 
-    for (sys) |name| {
-        var child = ChildProcess.init(&args, b.allocator);
+    const full_path = try std.process.getCwdAlloc(b.allocator);
 
-        const full_path = try std.process.getCwdAlloc(b.allocator);
-        // build cwd
-        const cwd = std.fs.path.join(b.allocator, &[_][]const u8{
-            full_path,
-            name,
-        }) catch |err| {
-            std.log.err("fmt path for examples failed, err is {}", .{err});
-            std.process.exit(1);
-        };
+    var dir = std.fs.openDirAbsolute(full_path, .{ .iterate = true }) catch |err| {
+        std.log.err("open path failed {s}, err is {}", .{ full_path, err });
+        std.process.exit(1);
+    };
+    defer dir.close();
 
-        // open entry dir
-        const entry_dir = std.fs.openDirAbsolute(cwd, .{}) catch unreachable;
-        entry_dir.access("build.zig", .{}) catch {
-            std.log.err("not found build.zig in path {s}", .{cwd});
-            std.process.exit(1);
-        };
+    var iterate = dir.iterate();
 
-        // set child cwd
-        // this api maybe changed in the future
-        child.cwd = cwd;
+    while (iterate.next()) |val| {
+        if (val) |entry| {
+            // get the entry name, entry can be file or directory
+            const name = entry.name;
+            if (entry.kind == .directory) {
+                if (eqlu8(name, ".zig-cache") or eqlu8(name, "zig-out") or eqlu8(name, "zig-cache"))
+                    continue;
 
-        // spawn and wait child process
-        _ = child.spawnAndWait() catch unreachable;
+                // build child process
+                var child = ChildProcess.init(&args, b.allocator);
+
+                // build cwd
+                const cwd = std.fs.path.join(b.allocator, &[_][]const u8{
+                    full_path,
+                    name,
+                }) catch |err| {
+                    std.log.err("fmt path failed, err is {}", .{err});
+                    std.process.exit(1);
+                };
+
+                // open entry dir
+                const entry_dir = std.fs.openDirAbsolute(cwd, .{}) catch unreachable;
+                entry_dir.access("build.zig", .{}) catch {
+                    std.log.err("not found build.zig in path {s}", .{cwd});
+                    std.process.exit(1);
+                };
+
+                // set child cwd
+                // this api maybe changed in the future
+                child.cwd = cwd;
+
+                // spawn and wait child process
+                _ = child.spawnAndWait() catch unreachable;
+            }
+        } else {
+            // Stop endless loop
+            break;
+        }
+    } else |err| {
+        std.log.err("iterate examples_path failed, err is {}", .{err});
+        std.process.exit(1);
     }
+}
+
+fn eqlu8(a: []const u8, b: []const u8) bool {
+    return std.mem.eql(u8, a, b);
 }
---
+++ course/code/12/build_system/tinytetris/build.zig
@@ -0,0 +1,57 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    // 构建目标
+    const target = b.standardTargetOptions(.{});
+
+    // 构建优化模式
+    const optimize = b.standardOptimizeOption(.{});
+
+    if (target.result.os.tag == .windows) {
+        return;
+    }
+
+    // 添加一个二进制可执行程序构建
+    // 注意：我们在这里并没有使用 root_source_file 字段
+    // 该字段是为 zig 源文件准备的
+    const exe = b.addExecutable(.{
+        .name = "zig",
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // 添加 C 源代码文件，两个参数：
+    // 源代码路径（相对于build.zig）
+    // 传递的 flags
+    // 多个 C 源代码文件可以使用 addCSourceFiles
+    exe.addCSourceFile(.{
+        .file = b.path("src/main.cc"),
+        .flags = &.{},
+    });
+
+    // 同理对于 C 标准库可以使用 linkLibC
+    // 链接C++ 标准库
+    exe.linkLibCpp();
+
+    // 链接系统库 ncurses
+    exe.linkSystemLibrary("ncurses");
+
+    // 添加到顶级 install step 中作为依赖
+    b.installArtifact(exe);
+
+    // 创建一个运行
+    const run_cmd = b.addRunArtifact(exe);
+
+    // 依赖于构建
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    // 运行时参数传递
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    // 运行的 step
+    const run_step = b.step("run", "Run the app");
+    // 依赖于前面的运行
+    run_step.dependOn(&run_cmd.step);
+}
