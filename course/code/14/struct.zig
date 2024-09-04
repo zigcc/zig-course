5ded293e619c302a59423368b3d3398f90806a51
jinzhongjia <mail@nvimer.org>
2024-09-04 17:31:56 +0800

    source file

--- course/code/14/struct.zig
+++ course/code/14/struct.zig
@@ -363,3 +363,21 @@ const PackedCast = struct {
     }
     // #endregion packed_cast
 };
+
+const aligned_struct = struct {
+    // #region aligned_struct
+    const std = @import("std");
+    const expect = std.testing.expect;
+
+    const S = packed struct {
+        a: u32,
+        b: u32,
+    };
+    test "overaligned pointer to packed struct" {
+        var foo: S align(4) = .{ .a = 1, .b = 2 };
+        const ptr: *align(4) S = &foo;
+        const ptr_to_b: *u32 = &ptr.b;
+        try expect(ptr_to_b.* == 2);
+    }
+    // #endregion aligned_struct
+};
