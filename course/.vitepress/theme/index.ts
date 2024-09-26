8d8b49ebd0faff79c02e250417675a9561e6962f
jinzhongjia <mail@nvimer.org>
2024-09-26 12:17:32 +0800

    fix：pdf 导出

--- course/.vitepress/theme/index.ts
+++ course/.vitepress/theme/index.ts
@@ -7,24 +7,25 @@ import "./style/print.css";
 
 import { h } from "vue";
 
-// 保证在打印时所有的 details 都是展开的
-window.addEventListener("beforeprint", function () {
-  document.querySelectorAll("details").forEach(function (details) {
-    details.setAttribute("open", "");
-  });
-});
-
-// 打印后重置 details 的展开状态
-window.addEventListener("afterprint", function () {
-  document.querySelectorAll("details").forEach(function (details) {
-    details.removeAttribute("open");
-  });
-});
-
 export default {
   ...DefaultTheme,
   enhanceApp(ctx: any) {
     DefaultTheme.enhanceApp(ctx);
+    if (typeof window != "undefined") {
+      // 保证在打印时所有的 details 都是展开的
+      window.addEventListener("beforeprint", function () {
+        document.querySelectorAll("details").forEach(function (details) {
+          details.setAttribute("open", "");
+        });
+      });
+
+      // 打印后重置 details 的展开状态
+      window.addEventListener("afterprint", function () {
+        document.querySelectorAll("details").forEach(function (details) {
+          details.removeAttribute("open");
+        });
+      });
+    }
   },
   Layout() {
     return h(DefaultTheme.Layout, null, {
